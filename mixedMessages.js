67a7caf57b3dbb96dc17273e93850d43ab8cc1f1
Batman1721 <jm.goncalvs.92@hotmail.com>
2021-01-30 10:58:57 +0000

    Codigo Final

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,1 +1,8 @@
-console.log('macaco');
+let first=['Eu sou', 'Tu és', 'Ele é'];
+let second=[' toto', ' bonito como um macaco', ' filho de Deus'];
+
+function randomSentence(texto){
+    return texto[Math.floor(Math.random()*texto.length)];
+};
+
+console.log(randomSentence(first)+randomSentence(second));
